7e40d1363e849e9f007a9e1703710a7dcdefc5b0
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 16:57:16 +0900

    Update ProgD2.java

--- src/main/java/ProgD2.java
+++ src/main/java/ProgD2.java
@@ -1,8 +1,8 @@
 public class ProgD2 {
 
-	public static void main(String[] args) {
-		RailwayLine yamanote = new RailwayLine();
-		System.out.println(yamanote.getStationsAsCSV());
-	}
+    public static void main(String[] args) {
+        RailwayLine yamanote = new RailwayLine();
+        System.out.println(yamanote.getStationsAsCSV());
+    }
 }
 
