08a80adc661aa29b4418087d8bd289e8d9dc9676
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-15 15:19:19 +0900

    Update ProgD2Test.java

--- src/test/java/ProgD2Test.java
+++ src/test/java/ProgD2Test.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class ProgD2Test {
 
     @Test
-    public void testHelloWorld()
+    public void testMain()
     {
         PrintStream originalOut = System.out;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -15,14 +15,12 @@ public class ProgD2Test {
         System.setIn(in);
 
         // action
-        // in.inputln("2"); // 標準入力をテストする場合
-        // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         ProgD2.main(null);
-
-        // assertion
-        assertEquals("Hello world!\n", bos.toString());
-
         // undo the binding in System
         System.setOut(originalOut);
+        
+        String prints[] = bos.toString().split("\r\n|\n", -1);
+        // assertion
+        assertEquals("東京,神田,秋葉原,御徒町,上野,鶯谷,日暮里,西日暮里,田端,駒込,巣鴨,大塚,池袋,目白,高田馬場,高田馬場,新大久保,新宿,代々木,原宿,渋谷,恵比寿,目黒,五反田,大崎,品川,田町,浜松町,新橋,ゆうらくちょう,", prints[0],"getStationsAsCSV()の出力が不正です!");
     }
 }
