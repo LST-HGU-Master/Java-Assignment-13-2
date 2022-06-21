70461b2a7836796f1338127b4a19d893e444a8a9
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-21 23:35:43 +0900

    Update RailwayLineTest.java

--- src/test/java/RailwayLineTest.java
+++ src/test/java/RailwayLineTest.java
@@ -7,7 +7,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
-
+/**
+ * @version (20220621)
+ */
 public class RailwayLineTest {
 
     ArrayList<String> stations = new ArrayList<String>(Arrays.asList(
@@ -44,7 +46,7 @@ public class RailwayLineTest {
     ));
 
     @Test
-    public void testPrintStations()
+    public void testGetStationsAsCSV()
     {
         try{
             // action
@@ -61,11 +63,11 @@ public class RailwayLineTest {
             // assertion
             String expected = "東京,神田,秋葉原,御徒町,上野,鶯谷,日暮里,西日暮里,田端,駒込,巣鴨,大塚,池袋,目白,高田馬場,高田馬場,新大久保,新宿,代々木,原宿,渋谷,恵比寿,目黒,五反田,大崎,品川,田町,浜松町,新橋,ゆうらくちょう,";
 
-            assertEquals(expected, y.getStationsAsCSV());
+            assertEquals(expected, y.getStationsAsCSV(), "getStationsAsCSV()の出力が期待したものと異なります!");
 
         } catch (Exception e) {
             // なにかしら例外がでたらfailにする
-            fail();
+            fail("教員に相談してください!");
         }
     }
 }
